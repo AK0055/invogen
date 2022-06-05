45919d7e3972ac23510a2b137a6421c403fd04dc
Ajaykumar M <m1ajay2001@gmail.com>
2022-06-05 21:00:20 +0530

    Login UI

---
+++ pages/firebaseconfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD4qJOO8ctXrDjrFOAqx0U-GR5juqlcaYU",
+  authDomain: "invogen-b0bc9.firebaseapp.com",
+  projectId: "invogen-b0bc9",
+  storageBucket: "invogen-b0bc9.appspot.com",
+  messagingSenderId: "242592679124",
+  appId: "1:242592679124:web:bddb278a8b61f49489a741"
+};
+
+
+export default firebaseConfig;
--- pages/login.js
+++ pages/login.js
@@ -2,9 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import {signInWithGoogle, anonySignIn} from './firelogin';
-import { useEffect,useState } from 'react';
 import Link from 'next/link'
-import {usern,setuser} from './firelogin'
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -16,17 +15,17 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a>Invogen</a>
+          Welcome back to <a>Invogen</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by Signing up!
+          Login to your account!
         </p>
 
         <div className={styles.grid}>
         <div class="p-4 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700">
         <form class="space-y-6" action="#">
-        <h5 class="text-xl font-medium text-gray-900 dark:text-white text-center">Create an account</h5>
+        <h5 class="text-xl font-medium text-gray-900 dark:text-white text-center">Log In</h5>
         <div>
             <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
             <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com"/>
@@ -48,14 +47,13 @@ export default function Home() {
                 </div>
             </div>
         </div>
-        <button type="submit" onClick={()=>{console.log(usern)}}class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
+        <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
         <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
-        Already have an account? 
-        <Link href="/login">
-        <a class="text-blue-700 hover:underline dark:text-blue-500">Log In</a>
+        New user?   
+        <Link href="/">
+            <a class="text-blue-700 hover:underline dark:text-blue-500">Sign Up</a>
         </Link>
         </div>
-        
     </form>
 </div>
         </div>
