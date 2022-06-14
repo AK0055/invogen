1fc7d61927f89f430b51e1e80d94f2f8ea2f3ad3
Ajaykumar M <m1ajay2001@gmail.com>
2022-06-14 10:19:35 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,8 +11,9 @@ module.exports = {
    './public/**/*.html',
    './src/**/*.{js,jsx,ts,tsx,vue}',
  ],
-  theme: {
-    // ...
-  }
-  // ...
+ eslint: {
+  // Warning: This allows production builds to successfully complete even if
+  // your project has ESLint errors.
+  ignoreDuringBuilds: true,
+},
 }
